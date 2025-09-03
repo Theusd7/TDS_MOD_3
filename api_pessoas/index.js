2a3ad81f545d4f01a704aff39d83c62bd77e9514
matheus <matheus.defaveri@escola.pr.gov.br>
2025-09-02 21:33:16 -0300

    adicionado put

--- api_pessoas/index.js
+++ api_pessoas/index.js
@@ -1,23 +1,28 @@
-const express = require("express") 
+const express = require("express")
 
 const app = express();
 
 app.use(express.json());
 
 const pessoas = [
-    {id : 1, nome: "louco", telefone: "46999356435"}];
+    { id: 1, nome: "louco", telefone: "46999356435" }];
 
-app.get("/pessoas",(request,response)=>{
+var contador = 1;
+
+app.get("/pessoas", (request, response) => {
     return response.send(pessoas)
 })
 
 
-app.post("/pessoas", (request,response)=>{
+app.post("/pessoas", (request, response) => {
     const nome = request.body.nome;
     const telefone = request.body.telefone;
     const status = request.body.status;
 
+    contador++;
+
     pessoas.push({
+        id: contador,
         nome,
         telefone,
         status
@@ -26,11 +31,38 @@ app.post("/pessoas", (request,response)=>{
 });
 
 
-app.get("/pessoas/:nome",(request,response) =>{
+app.get("/pessoas/:nome", (request, response) => {
     const nome = request.params.nome;
 
     return response.send(nome);
 });
-app.listen(8080,()=>{
+
+app.put("/pessoas", (request, response) => {
+    const { id, nome, telefone, status } = request.body;
+
+    console.log({ id, nome, telefone, status });
+
+    if (id == undefined) {
+        return response.send({
+            mensagem:
+                "o campo id é obrigatorio"
+        });
+    }
+
+    const pessoa = pessoas.filter((item) => item.id == id);
+
+    if(pessoa.length <= 0){
+        return response.send({mensagem:"pessoa invalida"});
+    }
+  
+    pessoa[0].nome = nome;
+    pessoa[0].telefone = telefone;
+    pessoa[0].status = status;
+    return response.send({ id, nome, telefone, status });
+});
+
+
+app.listen(8080, () => {
     console.log("o server esta rodando na porta 8080");
 });
+
